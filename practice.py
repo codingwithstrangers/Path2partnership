fe10df62f219c330533a418fd45a91e930668dd8
Heero <techheero@gmail.com>
2023-05-07 01:24:33 -0500

    Someone get this to work

--- practice.py
+++ practice.py
@@ -1,14 +1,23 @@
-from clientshit import access_token
+import asyncio
 import os
+from twitchio.ext import commands, routines
+from clientshit import access_token
+from twitchio.ext import commands
 
-class Bot(commands.Bot):
-    points_by_chatter = {}
-    
 
+class Hot_Tub_Partner(commands.bot):
+    viewer_activity = {}
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
-        
         super().__init__(token= access_token , prefix='?', initial_channels=['codingwithstrangers'],
-            nick = "Perfect_Stranger")
+            nick = "Perfect_Virewer")
+        
+        
+    async def event_ready(self):
+        # Notify us when everything is ready!
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
+        print("this shit works")
