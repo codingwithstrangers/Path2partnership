be09165d4c599c8ff6bef1fa8290e2dc7a3928b6
Heero <techheero@gmail.com>
2023-05-07 00:13:34 -0500

    Starting new Project Operation Partner

---
+++ practice.py
@@ -0,0 +1,14 @@
+from clientshit import access_token
+import os
+
+class Bot(commands.Bot):
+    points_by_chatter = {}
+    
+
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        
+        super().__init__(token= access_token , prefix='?', initial_channels=['codingwithstrangers'],
+            nick = "Perfect_Stranger")
