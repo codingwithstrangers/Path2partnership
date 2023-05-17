dbb929c25856251737db988330c4c80054cf4853
Heero <techheero@gmail.com>
2023-05-16 22:26:44 -0500

    A breakthrough maybe

--- practice.py
+++ practice.py
@@ -1,37 +1,18 @@
 from twitchio.ext import commands
+from clientshit import access_token
 
-class Bot (commands.Bot):
-    viewer_activity = {}
-    def __init__(self):
+class Bot(commands.Bot):
+
+   def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
+        
         super().__init__(token= access_token , prefix='?', initial_channels=['codingwithstrangers'],
-            nick = "Perfect_Virewer")
-        
-        
-    async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
-        print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
-        print("this shit works")
+            nick = "Strangest_Racer")
 
-        #this is how the routine starts
-        self.send_racer.start()
-
-    async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
-        if message.echo:
-            return
-        # Print the contents of our message to console...
-        print(message.content.encode("utf-8"))
-        print(message.author.name)
-    
+   
+print("this shit works")
 bot = Bot()
 bot.run()
-# bot.run() is blocking and will stop execution of any below code here until stopped or closed.     
-
-    
-
+# bot.run() is blocking and will stop execution of any below code here until stopped or closed.
--- twitchauth.py
+++ twitchauth.py
@@ -1,118 +1,41 @@
-import clientshit
 import requests
+from dotenv import load_dotenv
+import os
 
-# Set the grant type
-GRANT_TYPE = "client_credentials"
+# Load environment variables
+load_dotenv()
+client_id = os.getenv("TWITCH_CLIENT_ID")
+oauth_token = os.getenv(TWITCH_OAUTH_TOKEN)
 
-# Set the URL for the token request
-TOKEN_URL = "https://id.twitch.tv/oauth2/token"
-
-# Set the request headers
+# Set up API request headers
 headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Client-ID": clientshit.client_id,
-    "Client-SECRET": clientshit.client_id,
-    "Authorization": f"Basic {clientshit.client_secret}"
+    "Authorization": f"Bearer {oauth_token}",
+    "Client-Id": client_id
 }
 
-# Set the request data
-data = {
-    "grant_type": GRANT_TYPE,
-    "scope": "chat:read"
-}
+# Make API request to get user's channel information
+response = requests.get("https://api.twitch.tv/helix/users", headers=headers)
 
-# Make the request to generate a new access token
-response = requests.post(TOKEN_URL, headers=headers, data=data)
-
-# If the request is successful
+# Check if request was successful
 if response.status_code == 200:
-    # Extract the new access token
-    access_token = response.json()["access_token"]
-
-    # Update the clientshit module with the new access token
-    clientshit.access_token = access_token
-
-    print("Access token obtained successfully!")
-else:
-    print("Error obtaining access token")
-    print("Response status code:", response.status_code)
-    print("Response text:", response.text)
-
-# import requests
-# import time
-# import os
-
-# # Set the path to the clientshit.py file
-# CLIENTSHIT_PATH = "F:/Coding with Strangers/Twitchbot/perfectstrangerbot/clientshit.py"
-
-# # Set the grant type
-# GRANT_TYPE = "refresh_token"
-
-# # Set the URL for the token request
-# TOKEN_URL = "https://id.twitch.tv/oauth2/token"
-
-# # Load the client ID, client secret, and refresh token from the clientshit.py file
-# with open(CLIENTSHIT_PATH, "r") as f:
-#     lines = f.readlines()
-#     client_id = lines[0].strip()
-#     client_secret = lines[1].strip()
-#     refresh_token = lines[2].strip()
-
-# # Set the request headers
-# headers = {
-#     "Content-Type": "application/x-www-form-urlencoded",
-#     "Client-ID": client_id,
-#     "Authorization": f"Basic {client_secret}"
-# }
-
-# # Set the request data
-# data = {
-#     "grant_type": GRANT_TYPE,
-#     "refresh_token": refresh_token
-# }
-
-# # Set the refresh interval (in seconds)
-# REFRESH_INTERVAL = 3600  # refresh every hour
-
-# def refresh_access_token():
-#     """Function to refresh the access token"""
-#     # Make the request to generate a new access token
-#     response = requests.post(TOKEN_URL, headers=headers, data=data)
-# response = requests.post(TOKEN_URL, headers=headers, data=data)
-
-# # If the request is successful
-# if response.status_code == 200:
+    # Get user's channel ID from response
+    data = response.json()
+    user_id = data["data"][0]["id"]
     
-#     # Extract the new access token
-#     access_token = response.json()["access_token"]
-
-#     # Update the clientshit.py file with the new access token
-#     with open(CLIENTSHIT_PATH, "w") as f:
-#         f.write(f"client_id = {client_id}\n")
-#         f.write(f"client_secret = {client_secret}\n")
-#         f.write(f"access_token = {access_token}\n")
-
-#     print("Access token refreshed successfully!")
-# else:
-#     print("Error refreshing access token")
-
-# refresh_access_token()
-# import requests
-# from urllib.parse import urlencode
-# from clientshit import client_id , client_secret
-
-# query_string = urlencode({'client_id': client_id, 'client_secret': client_secret, "grant_type": "client_credentials"})
-
-# #r format is good for udeveloperxrl stringsand, a \ is a special character in a string
-# twitch_url_prefix = r'https://id.twitch.tv/oauth2/token?'
-
-# finished_endpoint = twitch_url_prefix + query_string
-
-# #meta data that I have to send behind the scenes with your post request
-# headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-# #Send the post request with the correct header 
-# respond = requests.post(finished_endpoint, headers=headers)
-
-# #print content contents (do off stream)
-# print(respond.content, "sockheadrps is the F####ing man")
+    # Make API request to get channel information
+    response = requests.get(f"https://api.twitch.tv/helix/channels?broadcaster_id={user_id}", headers=headers)
+    
+    # Check if request was successful
+    if response.status_code == 200:
+        # Get channel information from response
+        data = response.json()
+        channel_name = data["data"][0]["broadcaster_name"]
+        game_name = data["data"][0]["game_name"]
+        
+        # Print channel information
+        print(f"Channel Name: {channel_name}")
+        print(f"Currently Playing: {game_name}")
+    else:
+        print(f"Error retrieving channel information. Status code: {response.status_code}")
+else:
+    print(f"Error retrieving user information. Status code: {response.status_code}")
