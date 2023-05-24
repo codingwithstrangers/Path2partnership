5be59043aa103146c952e83ddf6bbec264187f10
Heero <techheero@gmail.com>
2023-05-24 00:52:50 -0500

    We got the follow and the shoutout to work!

--- practice.py
+++ practice.py
@@ -1,82 +1,72 @@
-from twitchio.ext import commands
-from dotenv import load_dotenv
+import logging; 
 import twitchio
-import channel_points
-import asyncio
-from twitchio.ext import pubsub
-import os
-import twitchio
+import datetime
 from twitchio.ext import commands, eventsub
-from dotenv import load_dotenv
-load_dotenv()
- 
-client_id = os.environ['CLIENT_ID']
-access_token = os.environ['ACCESS_TOKEN']
-client_secret = os.environ['CLIENT_SECRET']
-users_oauth_token = os.environ['USER_OAUTH_TOKEN']
-users_channel_id = int(os.environ['USER_CHANNEL_ID'])
-initial_channel = os.environ['INITIAL_CHANNEL']
-web_hook =os.environ['WEBHOOK']
-call_back = 'https://1443-72-133-128-106.ngrok-free.app'
- 
-client = twitchio.Client(token=client_id)
-client.pubsub = pubsub.PubSubPool(client)
-users_using_reward = set()
-REWARD_NAME = 'The Perfect Lurker'
-user_channel_id = 195345186
+from configuration import CLIENT_ID, CLIENT_SECRET, TOKEN, CALLBACK, CHANNEL_NAME, USER_CHANNEL_ID, MODERATOR_ID, WEBHOOK_SECRET, ESCLIENT_PORT
 
-esbot = commands.Bot.from_client_credentials(client_id= client_id, client_secret=client_secret)
-esclient = eventsub.EventSubClient(esbot, webhook_secret= web_hook, callback_route= call_back)
- 
+logging.basicConfig(level=logging.INFO) # Set this to DEBUG for more logging, INFO for regular logging
+logger = logging.getLogger("twitchio.http")
+
+
+#(Optional) 1. Go to https://dev.twitch.tv/console/apps/ and create an app
+#(Optional) 2. Download and install https://dev.twitch.tv/docs/cli/ 
+#(Optional) 3. Configure twitchcli command 'twitch configure' with client_id and _client_secret from step 1
+_CLIENT_ID = CLIENT_ID     # Application client_id from the twitch console dashboard 
+_CLIENT_SECRET = CLIENT_SECRET # App secret
+# 4.0 Register a new twitch account to be used for the bot
+# 4.1 Generate a token for the chatbot with scopes(twitchtokengenerator is fine for now): 
+#   twitch token -s 'chat:read user:read:follows'
+_TOKEN = TOKEN
+# 5.0 Register with the github account at https://ngrok.com
+# 5.1 Download and install ngrok on your local machine to create a websocket tunel
+# 5.2 Start ngrok with 'ngrok http 4000'. This will act as our webhook and the url hast to be the same as in the twitch developer console
+_CALLBACK = CALLBACK
+_CHANNEL_NAME = CHANNEL_NAME
+_USER_CHANNEL_ID = USER_CHANNEL_ID
+_MODERATOR_ID = MODERATOR_ID
+# 9. _WEBOOK_SECRET is a >10 digit random string. If this string changes then you need to reauthorize the twitch app for every (click the 'Click this' link again).
+_WEBHOOK_SECRET = WEBHOOK_SECRET
+# Sometimes you have to use the delete_all_active_subscriptions() function when you restart the bot
+# This is the port that the webhook will listen on locally
+_ESCLIENT_PORT = ESCLIENT_PORT
+
+# Simulate the event with twitchcli 'twitch event trigger channel.follow -s <_WEBHOOK_SECRET>  --from-user <_MODERATOR_ID> --to-user <_BROADCASTER_ID> --version 2 --forward-address <_CALLBACK>
+
+esbot = commands.Bot.from_client_credentials(client_id=_CLIENT_ID, client_secret=_CLIENT_SECRET)
+esclient = eventsub.EventSubClient(esbot, webhook_secret=_WEBHOOK_SECRET, callback_route=_CALLBACK)#, token=_TOKEN)
+
+
 class Bot(commands.Bot):
- 
     def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token=access_token, prefix='?', initial_channels=['codingwithstrangers'])
- 
-    async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
-        print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
-        print('this shit blows')
-
+        super().__init__(token=_TOKEN, prefix="?", initial_channels=_CHANNEL_NAME)
 
     async def __ainit__(self) -> None:
-        self.loop.create_task(esclient.listen(port=4040))
+        self.loop.create_task(esclient.listen(port=_ESCLIENT_PORT))
+#        await esclient.delete_all_active_subscriptions()
+#        logger.debug(f"Deleted all subscriptions")
         try:
-            await esclient.subscribe_channel_follows_v2(broadcaster= user_channel_id, moderator=user_channel_id)
-        except twitchio.HTTPException:
-            pass
+            await esclient.subscribe_channel_follows_v2(broadcaster=_USER_CHANNEL_ID, moderator=_MODERATOR_ID)
+            await esclient.subscribe_channel_shoutout_receive(broadcaster=_USER_CHANNEL_ID, moderator=_MODERATOR_ID)
+            await esclient.subscribe_channel_points_redeemed(broadcaster=_USER_CHANNEL_ID)
+            logger.debug(f"Subscribed esclient to subscribe_channel_follows_v2")
+        except twitchio.HTTPException as e:
+            logger.error(f"esclient failed to subscribe: {e}")
 
     async def event_ready(self):
-        print("Bot is ready!")
-
-    @esbot.event()
-    async def event_pubsub_channel_points(event: pubsub.PubSubChannelPointsMessage):
-        print(event)
-        pass # do stuff on channel point redemptions
-        print("Received event!")
-    async def __ainit__(self) -> None:
-            self.loop.create_task(esclient.listen(port=4040))
-            try:
-                await esclient.subscribe_channel_follows_v2(broadcaster= user_channel_id, moderator=user_channel_id)
-            except twitchio.HTTPException:
-                pass  
+        logger.info(f"Bot is ready!")
 
 bot = Bot()
 bot.loop.run_until_complete(bot.__ainit__())
 
-
-
+@esbot.event()
+async def event_eventsub_notification_channel_shoutout_receive(payload: eventsub.ChannelShoutoutReceiveData) -> None:
+    logger.info(f"{payload.data.started_at}, Shoutout Event, {payload.data.user.name}")
 
 @esbot.event()
 async def event_eventsub_notification_followV2(payload: eventsub.ChannelFollowData) -> None:
-    print("Received event!")
-    channel = bot.get_channel("codingwithstrangers")
-    await channel.send(f"{payload.data.user.name} followed woohoo!")        
-
+    logger.info(f"{payload.data.followed_at}, Follow Event, {payload.data.user.name}, {payload.data.broadcaster.name}") #this uses the payload timestamp instead
+#    channel = esbot.get_channel('channel')
+#    channel = esbot.get_channel(payload.data.broadcaster.name)
+#    await channel.send(f"{payload.data.user.name} followed KreyGasm!")
 
 bot.run()
